406404ef22b0cf883b9bea1175a4bab80f4d241f
yejf <490293266@qq.com>
2021-05-16 21:17:02 +0800

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<script lang="tsx">
+import { defineComponent } from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+
+export default defineComponent({
+  name: 'Home',
+
+  components: {
+    HelloWorld
+  },
+
+  // eslint-disable-next-line
+  setup() {
+    return () => (
+      <div class="home">
+        <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+      </div>
+    )
+  }
+})
+</script>
